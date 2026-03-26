5fcd615e5e1ccde74c28750614cc14aa504b50e5
xfoule <x.foule@gmail.com>
2026-03-25 23:16:49 -0400

    add contrôleur statut

---
+++ backend/controllers/statutController.js
@@ -0,0 +1,45 @@
+const StatutModel = require("../models/statutModel");
+
+const statutController = {
+    // Récupérer tous les statuts
+    getAll: async (req, res) => {
+        try {
+            const statuts = await StatutModel.getAll();
+            res.json(statuts);
+        } catch (error) {
+            res.status(500).json({ message: "Erreur lors de la récupération des statuts" });
+        }
+    },
+
+    // Ajouter un statut (Admin)
+    create: async (req, res) => {
+        try {
+            const id = await StatutModel.insert(req.body.nom);
+            res.status(201).json({ message: "Statut ajouté", id });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    },
+
+    // Modifier un statut (Admin)
+    update: async (req, res) => {
+        try {
+            await StatutModel.update(req.params.id, req.body.nom);
+            res.json({ message: "Statut modifié" });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    },
+
+    // Supprimer un statut (Admin)
+    delete: async (req, res) => {
+        try {
+            await StatutModel.delete(req.params.id);
+            res.json({ message: "Status supprimé" });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+};
+
+module.exports = statutController;
