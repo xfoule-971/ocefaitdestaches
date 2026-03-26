d33c42d0d807fcbaff7bf99b6b1f7e852f581116
xfoule <x.foule@gmail.com>
2026-03-25 22:25:53 -0400

    add le modele technique

---
+++ backend/models/techniqueModel.js
@@ -0,0 +1,67 @@
+const db = require("../config/db");
+
+/**
+ * Model Techniques
+ */
+const TechniqueModel = {
+
+    /**
+     * Récupérer toutes les techniques
+     */
+    getAll: async () => {
+        const [rows] = await db.execute(`
+            SELECT * FROM techniques 
+            ORDER BY nom ASC
+        `);
+        return rows;
+    },
+
+    /**
+     * Une technique par ID
+     */
+    getById: async (id) => {
+        const [rows] = await db.execute(`
+            SELECT * FROM techniques 
+            WHERE id = ?
+        `, [id]);
+        return rows[0] || null;
+    },
+
+    /**
+     * Ajouter une technique (Admin)
+     */
+    insert: async (nom) => {
+        const [result] = await db.execute(`
+            INSERT INTO techniques (nom)
+            VALUES (?)
+        `, [nom]);
+        
+        return result.insertId;
+    },
+
+    /**
+     * Modifier une technique (Admin)
+     */
+    update: async (id, nom) => {
+        const [result] = await db.execute(`
+            UPDATE techniques 
+            SET nom = ?
+            WHERE id = ?
+        `, [nom, id]);
+        
+        return result;
+    },
+
+    /**
+     * Supprimer une technique (Admin)
+     */
+    delete: async (id) => {
+        const [result] = await db.execute(`
+            DELETE FROM techniques WHERE id = ?
+        `, [id]);
+        
+        return result;
+    }
+};
+
+module.exports = TechniqueModel;
