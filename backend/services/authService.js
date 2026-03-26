868960ae8baa4d1edda440115c0c77388f6dd5a9
xfoule <x.foule@gmail.com>
2026-03-26 08:44:16 -0400

    add service authentification

---
+++ backend/services/authService.js
@@ -0,0 +1,56 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const AdminModel = require("../models/adminModel");
+
+/**
+ * Service d'authentification admin
+ */
+const authService = {
+
+    /**
+     * Connexion administrateur
+     * @param {string} identifiant
+     * @param {string} password
+     */
+    login: async (identifiant, password) => {
+
+        // Vérification des entrées
+        if (!identifiant || !password) {
+            throw new Error("Champs requis manquants");
+        }
+
+        // Recherche de l'utilisateur
+        const admin = await AdminModel.findByUsername(identifiant);
+
+        if (!admin) {
+            throw new Error("Identifiant incorrect");
+        }
+
+        // Vérification du mot de passe hashé
+        const isMatch = await bcrypt.compare(password, admin.mot_de_passe);
+
+        if (!isMatch) {
+            throw new Error("Mot de passe incorrect");
+        }
+
+        // Génération du token JWT
+        const token = jwt.sign(
+            {
+                id: admin.id,
+                identifiant: admin.identifiant
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "3h"
+            }
+        );
+
+        return {
+            token,
+            adminId: admin.id
+        };
+    }
+
+};
+
+module.exports = authService;
