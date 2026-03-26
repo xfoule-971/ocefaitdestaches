9d643cdb6852e2b2b1ca26d685c91ec6346fa086
xfoule <x.foule@gmail.com>
2026-03-25 21:41:24 -0400

    add serveur port 4000

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const app = require("./app");
+
+const PORT = process.env.PORT || 4000;
+
+/**
+ * Lancement serveur
+ */
+app.listen(PORT, () => {
+    console.log(`API lancée sur http://localhost:${PORT}`);
+});
