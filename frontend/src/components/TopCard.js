39488d59c6fed0aa0b3617f1c190df6d66975570
xfoule <x.foule@gmail.com>
2026-03-26 18:53:57 -0400

    add composant topcard et paga d'accueil

--- frontend/src/components/TopCard.js
+++ frontend/src/components/TopCard.js
@@ -5,20 +5,27 @@ const TopCard = ({ oeuvre }) => {
     const imageUrl =`http://localhost:4000/uploads/${oeuvre.nom_fichier}`
 
     return (
-        
-        <div className="card bg-success text-light h-100 shadow-sm">
+
+        <div 
+            className="card bg-success text-light h-100 shadow-sm border border-5 border-warning" 
+            style={{ 
+                width: "550px",
+                borderRadius: "0"
+            }}
+        >
             
-            <Link>
+            <Link className="text-decoration-none text-light">
 
                 <img
                     src={imageUrl}
                     className="card-img-top img-fluid"
                     alt={oeuvre.titre}
+                    style={{height: "500px", objectFit: "cover"}}
                 />
 
                 <div className="card-body text-center d-flex flex-column">
 
-                    <h3 className="card-title text-truncate">{oeuvre.titre}</h3>
+                    <h3 className="card-title fw-semibold">{oeuvre.titre}</h3>
 
                 </div>
 
