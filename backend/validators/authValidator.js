87c3cd2ce7db8101c3d7a76bc3f281d1f98477e4
xfoule <x.foule@gmail.com>
2026-03-26 08:38:51 -0400

    add validateur administrateur

---
+++ backend/validators/authValidator.js
@@ -0,0 +1,42 @@
+const { body, validationResult } = require("express-validator");
+
+/**
+ * Validator Auth
+ */
+const authValidator = {
+
+    login: [
+
+        // Identifiant
+        body("identifiant")
+            .notEmpty().withMessage("Le nom d'utilisateur est requis")
+            .isString().withMessage("Format invalide")
+            .trim()
+            .escape(),
+
+        // Mote de passe
+        body("Mot_de_passe")
+            .notEmpty().withMessage("Le mot de passe est requis")
+            .isLength({ min: 6 }).withMessage("Minimum 6 caractères"),
+
+        /**
+         * Gestion des erreurs
+         */
+        (req, res, next) => {
+
+            const errors = validationResult(req);
+
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    success: false,
+                    errors: errors.array().map(err => err.msg)
+                });
+            }
+
+            next();
+        }
+
+    ]
+};
+
+module.exports = authValidator;
