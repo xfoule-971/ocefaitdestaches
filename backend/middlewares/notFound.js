0eef30e9db6cc9b6643f6b5f2e0efa3dcdeb64a7
xfoule <x.foule@gmail.com>
2026-03-26 08:11:10 -0400

    add 404

---
+++ backend/middlewares/notFound.js
@@ -0,0 +1,14 @@
+// Middleware 404
+const notFound = (req, res) => {
+
+    res.status(404).json ({
+
+        success: false,
+        
+        message: "Route non trouvée"
+
+    });
+
+};
+
+module.exports = notFound;
