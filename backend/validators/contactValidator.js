f9b90b289609ccef835e151678bd060ae632e850
xfoule <x.foule@gmail.com>
2026-03-26 10:36:10 -0400

    add ensemble des validateurs

---
+++ backend/validators/contactValidator.js
@@ -0,0 +1,42 @@
+const { body, validationResult } = require("express-validator");
+
+const contactValidator = {
+
+    send: [
+
+        body("nom")
+            .trim()
+            .notEmpty().withMessage("Le nom est requis")
+            .isLength({ min: 2 }).withMessage("Min 2 caractères"),
+
+        body("email")
+            .trim()
+            .notEmpty().withMessage("Email requis")
+            .isEmail().withMessage("Email invalide")
+            .normalizeEmail(),
+
+        body("message")
+            .trim()
+            .notEmpty().withMessage("Message requis")
+            .isLength({ min: 10 }).withMessage("Min 10 caractères"),
+
+        (req, res, next) => {
+
+            const errors = validationResult(req);
+
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    success: false,
+                    errors: errors.array().map(err => ({
+                        field: err.path,
+                        message: err.msg
+                    }))
+                });
+            }
+
+            next();
+        }
+    ]
+};
+
+module.exports = contactValidator;
