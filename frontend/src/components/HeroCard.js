ed4ccc8a40ecae3345af4a6da5b84146fbd44b13
xfoule <x.foule@gmail.com>
2026-03-26 16:22:47 -0400

    add creation composant herocard

---
+++ frontend/src/components/HeroCard.js
@@ -0,0 +1,21 @@
+const HeroCard = ({ title }) => {
+
+    return (
+
+        <div className="p-3 hero" style={{ zIndex: '1'}}>
+
+            <div className="text-center" style={{ zIndex: '999'}}>
+
+                <h1 className="text-light fw-bold text-center hero__title" style={{ fontSize : '66px'}}>
+                    {title}
+                </h1>
+
+            </div>
+
+        </div>
+
+    );
+
+};
+
+export default HeroCard;
